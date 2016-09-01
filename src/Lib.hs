ac4cfb692894613c0c80b2ac360bf3701cf23fef
Matthew Franglen <matthew@franglen.org>
2016-09-01 22:07:59 +0100

    Define MD3 record

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,28 @@
 module Lib
-    ( someFunc
+    ( 
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+data MD3 {
+      version :: Int
+    , name :: String
+    , flags :: Int
+    , frameCount :: Int
+    , tagCount :: Int
+    , surfaceCount :: Int
+    , skinCount :: Int
+    , frameOffset :: Int
+    , surfaceOffset :: Int
+    , size :: Int
+    , frames :: [Frame]
+    , tags :: [Tag]
+    , surfaces :: [Surface]
+    }
+
+data Frame {
+    }
+
+data Tag {
+    }
+
+data Surface {
+    }
