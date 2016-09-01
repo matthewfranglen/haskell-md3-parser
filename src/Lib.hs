b4162408daf4027586af4d767b2424337d7fc8a0
Matthew Franglen <matthew@franglen.org>
2016-09-01 23:38:21 +0100

    Writing some parsers

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,8 +2,9 @@ module Lib where
 
 import qualified Data.ByteString.Lazy.Char8 as L8
 import qualified Data.ByteString.Lazy as L
+import Data.Bits ((.|.), shift)
 import Data.Char (chr)
-import Data.Int (Int64)
+import Data.Int (Int16, Int64)
 import Data.Word (Word8)
 
 data MD3 = MD3 {
@@ -36,9 +37,35 @@ data ParseState = ParseState {
     , offset :: Int64
     }
 
+takeOne :: L.ByteString -> Maybe (Word8, L.ByteString)
+takeOne = L.uncons
+
+takeTwo :: L.ByteString -> Maybe ((Word8, Word8), L.ByteString)
+takeTwo xs = if length x' == 2
+             then Just ((x' !! 0, x' !! 1), xs')
+             else Nothing
+    where (x, xs') = L.splitAt 2 xs
+          x'       = L.unpack x
+
+takeFour :: L.ByteString -> Maybe ((Word8, Word8, Word8, Word8), L.ByteString)
+takeFour xs = if length x' == 4
+             then Just ((x' !! 0, x' !! 1, x' !! 2, x' !! 3), xs')
+             else Nothing
+    where (x, xs') = L.splitAt 4 xs
+          x'       = L.unpack x
+
 toU8 :: Word8 -> Char
 toU8 = chr . fromIntegral
 
 takeU8 :: L.ByteString -> Maybe (Char, L.ByteString)
-takeU8 xs = f <$> L.uncons xs
+takeU8 xs = f <$> takeOne xs
     where f (x', xs') = (toU8 x', xs')
+
+toS16 :: (Word8, Word8) -> Int16
+toS16 (x, y) = (shift x' 8) .|. y'
+    where x' = fromIntegral x :: Int16
+          y' = fromIntegral y :: Int16
+
+takeS16 :: L.ByteString -> Maybe (Int16, L.ByteString)
+takeS16 xs = f <$> takeTwo xs
+    where f (x, xs') = (toS16 x, xs')
